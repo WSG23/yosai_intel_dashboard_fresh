9fae9594e81b3678360cc4494cd863f10de3b35f
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-07-04 17:10:01 +0900

    build: generate both minified and unminified css

---
+++ scripts/build_css.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+set -e
+NODE_ENV=development postcss assets/css/main.css -o assets/dist/main.css
+printf '/* This file is auto-generated. Do not edit directly. */\n' | cat - assets/dist/main.css > assets/dist/main.css.tmp
+mv assets/dist/main.css.tmp assets/dist/main.css
+NODE_ENV=production postcss assets/css/main.css -o assets/dist/main.min.css --map
