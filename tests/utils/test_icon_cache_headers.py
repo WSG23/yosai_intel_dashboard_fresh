9254cca70fed4b57cd474206c7b3022a48fdda80
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-07-09 11:02:57 +0900

    Add cache header docs and tests

---
+++ tests/utils/test_icon_cache_headers.py
@@ -0,0 +1,28 @@
+import pytest
+from flask import Flask, Response
+
+from utils.assets_utils import ensure_icon_cache_headers
+
+
+def _make_app():
+    flask_app = Flask(__name__)
+
+    class DummyDash:
+        server = flask_app
+
+    return flask_app, DummyDash()
+
+
+def test_icon_cache_headers():
+    flask_app, dummy = _make_app()
+
+    @flask_app.route('/assets/navbar_icons/foo.png')
+    def icon():
+        return Response(b'data', content_type='image/png')
+
+    ensure_icon_cache_headers(dummy)
+
+    client = flask_app.test_client()
+    res = client.get('/assets/navbar_icons/foo.png')
+    assert res.headers.get('Cache-Control') == 'public, max-age=3600'
+    assert 'ETag' in res.headers
