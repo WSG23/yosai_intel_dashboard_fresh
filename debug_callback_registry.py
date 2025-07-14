120cb63ee1092144453f3cc7cff4ff2ae6c79b02
WSG23 <trb427@gmail.com>
2025-07-15 01:03:43 +0900

    Removal

--- debug_callback_registry.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Backward-compatible wrapper for callback diagnostics."""
-
-from tools.debug.callbacks import debug_callback_conflicts, validate_callback_system
-
-if __name__ == "__main__":  # pragma: no cover - manual tool
-
-    debug_callback_conflicts()
-    validate_callback_system()
--- gunicorn.conf.py
+++
@@ -1,6 +0,0 @@
-from config.constants import DEFAULT_APP_PORT
-
-bind = f"0.0.0.0:{DEFAULT_APP_PORT}"
-workers = 4
-threads = 2
-loglevel = "info"
