35349089cda72dae1d9ae32a32a463fea23b601d
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-07-15 08:12:58 +0900

    Remove unused root scripts

--- ui_callback_controller.py
+++
@@ -1,8 +0,0 @@
-from core.callback_events import CallbackEvent
-from core.callback_manager import CallbackManager
-
-UIEvent = CallbackEvent
-UICallbackController = CallbackManager
-ui_callback_controller = CallbackManager()
-
-__all__ = ["UIEvent", "UICallbackController", "ui_callback_controller"]
