12f33322fb4a01bbb6de5e5c694593fdc3e25978
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-07-16 08:21:51 +0900

    Refactor analytics callbacks to use controllers

--- analytics/controllers/__init__.py
+++ analytics/controllers/__init__.py
@@ -1,5 +1,27 @@
 """Analytics controller utilities."""
 
+from .deep_analytics_logic import (
+    dispatch_analysis,
+    get_data_sources,
+    get_initial_message_safe,
+    run_quality_analysis,
+    run_service_analysis,
+    run_suggests_analysis,
+    run_unique_patterns_analysis,
+    update_status_alert,
+)
+from .realtime_ws import RealTimeWebSocketController
 from .unified_controller import UnifiedAnalyticsController
 
-__all__ = ["UnifiedAnalyticsController"]
+__all__ = [
+    "UnifiedAnalyticsController",
+    "RealTimeWebSocketController",
+    "dispatch_analysis",
+    "run_quality_analysis",
+    "run_service_analysis",
+    "run_suggests_analysis",
+    "run_unique_patterns_analysis",
+    "update_status_alert",
+    "get_data_sources",
+    "get_initial_message_safe",
+]
