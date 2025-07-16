98c6cd0eea469e568472e932c5c0c1c2c877598d
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-07-17 01:10:36 +0900

    Add repeated access denied anomaly detection

--- analytics/security_patterns/__init__.py
+++ analytics/security_patterns/__init__.py
@@ -21,6 +21,7 @@ from .pattern_detection import (
     detect_pattern_threats,
     detect_rapid_attempts,
 )
+from .no_access_detection import detect_no_access_anomalies
 from .statistical_detection import (
     detect_failure_rate_anomalies,
     detect_frequency_anomalies,
@@ -44,6 +45,7 @@ __all__ = [
     "detect_pattern_threats",
     "detect_rapid_attempts",
     "detect_after_hours_anomalies",
+    "detect_no_access_anomalies",
     "ThreatIndicator",
 ]
 
