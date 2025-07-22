e260f87688f8d12f80185d2476c1773833141960
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-07-22 15:05:23 +0900

    Add real-time monitoring React page

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -4,3 +4,4 @@ export { default as Graphs } from './Graphs';
 export { default as Export } from './Export';
 export { default as Settings } from './Settings';
 export { default as RealTimeAnalyticsPage } from './RealTimeAnalyticsPage';
+export { default as RealTimeMonitoring } from './RealTimeMonitoring';
