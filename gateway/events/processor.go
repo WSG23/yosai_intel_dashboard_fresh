9caa6e499b0ed16fcacc28a9a0e88d566e2d8d45
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-07-22 16:30:04 +0900

    Add OpenTelemetry tracing with Jaeger

--- gateway/events/processor.go
+++ gateway/events/processor.go
@@ -3,6 +3,7 @@ package events
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"log"
 	"time"
 
@@ -48,12 +49,12 @@ func (ep *EventProcessor) Close() {
 	}
 }
 
-func (ep *EventProcessor) ProcessAccessEvent(event AccessEvent) error {
+func (ep *EventProcessor) ProcessAccessEvent(ctx context.Context, event AccessEvent) error {
 	if ep.engine == nil {
 		return errors.New("rule engine not configured")
 	}
 
-	dec, err := ep.engine.EvaluateAccess(context.Background(), event.PersonID, event.DoorID)
+	dec, err := ep.engine.EvaluateAccess(ctx, event.PersonID, event.DoorID)
 	if err != nil {
 		return err
 	}
@@ -63,7 +64,7 @@ func (ep *EventProcessor) ProcessAccessEvent(event AccessEvent) error {
 
 	// store decision in cache if available
 	if ep.cache != nil {
-		if err := ep.cache.SetDecision(context.Background(), cache.Decision{
+		if err := ep.cache.SetDecision(ctx, cache.Decision{
 			PersonID: event.PersonID,
 			DoorID:   event.DoorID,
 			Decision: event.AccessResult,
