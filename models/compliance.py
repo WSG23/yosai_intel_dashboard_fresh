ec6a44bea89af73be16dcf6fa85a7756fc1f2f6b
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-07-24 16:12:42 +0900

    Use explicit imports for compliance models

--- models/compliance.py
+++ models/compliance.py
@@ -1,1 +1,25 @@
-from plugins.compliance_plugin.models.compliance import *
+"""Expose compliance models used across the application."""
+
+from plugins.compliance_plugin.models.compliance import (
+    ConsentLog,
+    ConsentType,
+    DataSensitivityLevel,
+    DSARRequest,
+    DSARRequestType,
+    DSARStatus,
+    ComplianceAuditLog,
+    DataRetentionPolicy,
+    CREATE_COMPLIANCE_TABLES_SQL,
+)
+
+__all__ = [
+    "ConsentLog",
+    "ConsentType",
+    "DataSensitivityLevel",
+    "DSARRequest",
+    "DSARRequestType",
+    "DSARStatus",
+    "ComplianceAuditLog",
+    "DataRetentionPolicy",
+    "CREATE_COMPLIANCE_TABLES_SQL",
+]
