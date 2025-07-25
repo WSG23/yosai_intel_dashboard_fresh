0bf6369f92f01e98d28c4e1cae616dbcba81ab16
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-07-26 00:13:07 +0900

    Add unified DataValidator and integrate with analytics

--- validation/__init__.py
+++ validation/__init__.py
@@ -4,6 +4,7 @@ from .core import ValidationResult, Validator
 from .factory import create_file_validator, create_security_validator
 from .file_validator import FileValidator
 from .security_validator import SecurityValidator
+from .data_validator import DataValidator, DataValidatorProtocol
 from .rules import CompositeValidator, ValidationRule
 
 __all__ = [
@@ -13,6 +14,8 @@ __all__ = [
     "CompositeValidator",
     "SecurityValidator",
     "FileValidator",
+    "DataValidator",
+    "DataValidatorProtocol",
     "create_file_validator",
     "create_security_validator",
 ]
