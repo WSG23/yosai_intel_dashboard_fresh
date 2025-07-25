9392a18dac8bf0e064cda176a34e22067b0d4b97
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-07-25 19:58:01 +0900

    Add async tests

---
+++ tests/test_async_worker.py
@@ -0,0 +1,26 @@
+import asyncio
+import pytest
+
+class Worker:
+    def __init__(self):
+        self._task = None
+        self.ran = False
+
+    async def _job(self):
+        self.ran = True
+
+    async def start(self):
+        if self._task is None:
+            self._task = asyncio.create_task(self._job())
+
+    async def stop(self):
+        if self._task:
+            await self._task
+            self._task = None
+
+@pytest.mark.asyncio
+async def test_worker_schedule():
+    w = Worker()
+    await w.start()
+    await w.stop()
+    assert w.ran is True
