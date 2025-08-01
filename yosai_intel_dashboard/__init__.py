1a27dae1390fa550fb3890daa3d46335fffe0c9f
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-08-01 08:30:20 +0100

    fix package import path

--- yosai_intel_dashboard/__init__.py
+++ yosai_intel_dashboard/__init__.py
@@ -1,8 +1,8 @@
 import sys
 
 # Provide backward compatibility for old import paths
-import models as _models
-import src as _src
+from . import models as _models
+from . import src as _src
 
 sys.modules.setdefault(__name__ + ".models", _models)
 sys.modules.setdefault(__name__ + ".src", _src)
