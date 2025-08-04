7caf2de38202c173d92f7df6c36b29c30e6b8252
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-08-04 19:44:36 +0100

    Include TypeScript files in PurgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -2,13 +2,13 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('tailwindcss-react-native/postcss'),
-    require('tailwindcss'),
+    require('@tailwindcss/postcss'),
     require('autoprefixer'),
     process.env.NODE_ENV === 'production' &&
       require('cssnano')({ preset: 'default' }),
     process.env.NODE_ENV === 'production' &&
-      require('@fullhuman/postcss-purgecss')({
-        content: ['./**/*.py', './**/*.html', './**/*.js', './**/*.tsx'],
+      require('@fullhuman/postcss-purgecss').default({
+        content: ['./**/*.py', './**/*.html', './**/*.js', './**/*.ts', './**/*.tsx'],
         defaultExtractor: content =>
           content.match(/[^\s]*[A-Za-z0-9-_:/]+/g) || []
       })
