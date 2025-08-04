e9c19b66bf40e728cdf8c53e09dbd71c4aa0ed1e
WSG23 <154938577+WSG23@users.noreply.github.com>
2025-08-04 22:33:39 +0100

    refactor: update upload analytics import

--- yosai_intel_dashboard/src/services/controllers/upload_controller.py
+++ yosai_intel_dashboard/src/services/controllers/upload_controller.py
@@ -3,7 +3,7 @@ from typing import Any, Dict, List
 import pandas as pd
 
 from yosai_intel_dashboard.src.core.di_decorators import inject, injectable
-from yosai_intel_dashboard.src.services.analytics.upload_analytics import UploadAnalyticsProcessor
+from yosai_intel_dashboard.src.services.upload_processing import UploadAnalyticsProcessor
 from yosai_intel_dashboard.src.services.protocols.processor import ProcessorProtocol
 from yosai_intel_dashboard.src.services.upload.protocols import UploadAnalyticsProtocol
 from yosai_intel_dashboard.src.core.interfaces.service_protocols import UploadDataServiceProtocol
