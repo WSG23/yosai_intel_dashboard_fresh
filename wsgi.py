b9ff19903d1094dfe2c6f29536f5323f8126a330
WSG23 <trb427@gmail.com>
2025-08-15 07:02:29 +0900

    wsgi update

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,5 @@
 from __future__ import annotations
-
-import os
-import sys
-
+import os, sys
 try:
     from yosai_intel_dashboard.src.infrastructure.monitoring.logging_utils import get_logger
 except Exception:
@@ -12,11 +9,8 @@ except Exception:
         if not logger.handlers:
             logging.basicConfig(level=logging.INFO)
         return logger
-
 logger = get_logger(__name__)
-
 dev_mode = os.environ.get("YOSAI_DEV") == "1" or "--dev" in sys.argv
-
 if dev_mode:
     from yosai_intel_dashboard.src.core.app_factory import create_app as _create_app
     application = _create_app()
