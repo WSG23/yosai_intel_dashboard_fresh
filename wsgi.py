60697fb1f4460c6d3ad64606b45137059ff9b4a3
WSG23 <trb427@gmail.com>
2025-08-15 06:54:43 +0900

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
-import os, sys
+
+import os
+import sys
+
 try:
     from yosai_intel_dashboard.src.infrastructure.monitoring.logging_utils import get_logger
 except Exception:
@@ -10,11 +13,13 @@ except Exception:
             logging.basicConfig(level=logging.INFO)
         return logger
 
-from yosai_intel_dashboard.src.core.app_factory import create_app as _create_app
+logger = get_logger(__name__)
 
 dev_mode = os.environ.get("YOSAI_DEV") == "1" or "--dev" in sys.argv
-logger = get_logger(__name__)
-if dev_mode:
-    logger.info("Starting in dev mode")
 
-app = _create_app()
+if dev_mode:
+    from yosai_intel_dashboard.src.core.app_factory import create_app as _create_app
+    application = _create_app()
+else:
+    from yosai_intel_dashboard.src.adapters.api.adapter import create_api_app
+    application = create_api_app()
